aad8c95b79777697c820ab0a525eb98723434a10
Shivam <shivamdnngsp@gmail.com>
2026-06-29 23:23:24 +0530

    update PORT for railways

--- socket-server/server/app.ts
+++ socket-server/server/app.ts
@@ -76,6 +76,8 @@ socket.on("disconnect", () => {
 
 });
 
-server.listen(5000, () => {
-  console.log("Socket server running on 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Socket server running on ${PORT}`);
 });
