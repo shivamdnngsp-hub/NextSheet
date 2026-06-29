ecff0cb61d52446b8081f2544b095addf393d34b
Shivam <shivamdnngsp@gmail.com>
2026-06-29 23:21:05 +0530

    fix cors for deployment

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
+  transports: ["websocket"],
+  withCredentials: true,
+});
